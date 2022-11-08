f359e364cb2511a8cb585ca9c4112b78123c88dd
George Marselino Romero <gromero@profilesw.com>
2022-11-08 12:11:39 +0200

    added landing page and first basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+
+import { environment } from '@env/environment';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+	StoreModule.forRoot({}),
+	EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production, name: 'Bug Reporting System Devtools' })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/top-nav-bar/top-nav-bar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'brs-top-nav-bar',
+	templateUrl: './top-nav-bar.component.html',
+	styleUrls: ['./top-nav-bar.component.scss']
+})
+export class TopNavBarComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit(): void {
+	}
+
+
+}
