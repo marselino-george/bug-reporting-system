0b0e162e403e5827ee6ee0dabd39062541a6e1f6
George Marselino Romero <gromero@profilesw.com>
2022-11-09 02:15:08 +0200

    added new packages fontAwesome toastr json-server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
+import { ToastrModule } from 'ngx-toastr';
 
 import { environment } from '@env/environment';
 import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from './core/auth/auth.module';
 
 import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
 import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -20,10 +24,14 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
+	BrowserAnimationsModule,
+	HttpClientModule,
+	AuthModule,
     AppRoutingModule,
 	StoreModule.forRoot({}),
 	EffectsModule.forRoot([]),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production, name: 'Bug Reporting System Devtools' })
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production, name: 'Bug Reporting System Devtools' }),
+	ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
