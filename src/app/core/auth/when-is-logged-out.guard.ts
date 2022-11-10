a11d17118f277ddb529cf1cc75acd8809bd65d1b
George Marselino Romero <gromero@profilesw.com>
2022-11-10 17:38:59 +0200

    fix bug in loggin guard that prevents loggin after logout

--- src/app/core/auth/when-is-logged-out.guard.ts
+++ src/app/core/auth/when-is-logged-out.guard.ts
@@ -13,7 +13,7 @@ export class WhenIsLoggedOutGuard implements CanActivate {
 	canActivate(
 		route: ActivatedRouteSnapshot,
 		state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-		return this.authService.isLoggedIn();
+		return !this.authService.isLoggedIn();
 	}
 
 }
